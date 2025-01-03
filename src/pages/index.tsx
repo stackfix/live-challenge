d09842c95e1c37a1a63eecabe190375a8982033b
Rafiu <rafiu@aline.co>
2025-01-03 11:57:40 +0530

    made some progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
-import Head from "next/head";
+import { ProductCard } from "~/components/ProductCard";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  // const { data: products } = api.product.getAll.useQuery()
+  const { data: products, isLoading } = api.product.getAll.useQuery();
 
   return (
-    <>
-      <Head>
-        <title>Stackfix</title>
-        <meta name="description" content="stackfix.com" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="p-5">
-        <h1 className="text-xl">Stackfix Live Challenge</h1>
-        {/* TODO: Your code here */}
-      </main>
-    </>
+    <div className="container mx-auto space-y-4 p-4">
+      {isLoading
+        ? // Show loading states
+          Array(3)
+            .fill(0)
+            .map((_, i) => (
+              <ProductCard key={i} product={{} as any} isLoading={true} />
+            ))
+        : products?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+    </div>
   );
 }
