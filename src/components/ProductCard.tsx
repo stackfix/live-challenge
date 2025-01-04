dd86b67335788246fe4ab6bb2ea0584e9f3416bb
Rafiu <rafiu@aline.co>
2025-01-04 15:57:40 +0530

    adeded table view

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,4 @@
 import {
-  ArrowRightIcon,
   ExternalLinkIcon,
   InfoCircledIcon,
   TriangleRightIcon,
@@ -16,7 +15,55 @@ interface ProductCardProps {
   isLoading?: boolean;
 }
 const FlowChartConnector = () => (
-  <ArrowRightIcon className="h-4 w-4 text-gray-200" />
+  <svg
+    width="24"
+    height="28"
+    viewBox="0 0 24 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-gray-200"
+  >
+    <g clipPath="url(#clip0_3692_307467)">
+      <line
+        x1="11.5"
+        y1="26.9219"
+        x2="11.5"
+        y2="1.11328"
+        stroke="currentColor"
+      />
+      <line
+        x1="11.0547"
+        y1="26.6953"
+        x2="25.875"
+        y2="26.6953"
+        stroke="currentColor"
+      />
+      <line
+        x1="11.0547"
+        y1="0.695312"
+        x2="25.875"
+        y2="0.695312"
+        stroke="currentColor"
+      />
+      <line
+        x1="-0.546875"
+        y1="14.5"
+        x2="11.0625"
+        y2="14.5"
+        stroke="currentColor"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_3692_307467">
+        <rect
+          width="24"
+          height="28"
+          fill="white"
+          transform="matrix(-1 0 0 1 24 0)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
 );
 
 // Left Section - Logo
