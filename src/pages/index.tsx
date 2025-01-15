1fc3719125a2b5f21b0e86ef8c6650da6ac78c5c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-15 16:07:59 +0000

    feat(index.tsx): search field and sort field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useMemo, useState } from "react";
 import { Input } from "~/components/ui/input";
+import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group"
 import ProductCard from "~/components/ui/ProductCard/ProductCard";
 import ProductCardSkeleton from "~/components/ui/ProductCard/ProductCardSkeleton";
 import type { Product } from "~/server/api/routers/product/types";
@@ -23,11 +24,28 @@ export default function Home() {
   const { data: products } = api.product.getAll.useQuery()
 
   const [searchParam, setSearchParam] = useState('')
+  const [sortParam, setSortParam] = useState<'totalPrice' | 'fitScore' | 'stackFixScore' | undefined>(undefined)
+
   const filteredProducts = useMemo(() => {
     if (!products) return null
-    if (!searchParam) return products
-    return products.filter(product => product?.name?.includes(searchParam));
-  }, [products, searchParam])
+    return products
+      .filter(product => product?.name?.toLowerCase().includes(searchParam.toLowerCase()))
+      .sort((a, b) => {
+      switch (sortParam) {
+      case undefined:
+        return 0;
+        case 'totalPrice':
+          return a.pricing.totalPrice - b.pricing.totalPrice;
+        case 'fitScore':
+          return a.productScoring.fitScore - b.productScoring.fitScore;
+        case 'stackFixScore':
+          return a.productScoring.stackfixScore - b.productScoring.stackfixScore;
+        default:
+          sortParam satisfies never;
+          return 0;
+      }
+    })
+}, [products, searchParam, sortParam])
 
   return (
     <>
@@ -40,6 +58,13 @@ export default function Home() {
         <h1 className="text-xl">Stackfix Live Challenge</h1>
         {/* TODO: Your code here */}
         <Input placeholder="Search for products" value={searchParam} onChange={(e) => setSearchParam(e.target.value)}/>
+        {/* TODO fix type */}
+        <ToggleGroup type="single" onValueChange={(value) => setSortParam(value)}>
+          <ToggleGroupItem value="totalPrice">Total Price</ToggleGroupItem>
+          <ToggleGroupItem value="fitScore">Fit Score</ToggleGroupItem>
+          <ToggleGroupItem value="stackFixScore">StackFix Score</ToggleGroupItem>
+        </ToggleGroup>
+
         <div className="w-full max-w-4xl">
           {
             filteredProducts == null 
