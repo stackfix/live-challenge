42f3a12748b9db64b4b15d8c0914f202c9a0e445
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-15 15:52:58 +0000

    chore: implement search field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { useMemo, useState } from "react";
+import { Input } from "~/components/ui/input";
 import ProductCard from "~/components/ui/ProductCard/ProductCard";
 import ProductCardSkeleton from "~/components/ui/ProductCard/ProductCardSkeleton";
 import type { Product } from "~/server/api/routers/product/types";
@@ -20,6 +22,13 @@ export function ProductCards({ products }: {products: Product[]}) {
 export default function Home() {
   const { data: products } = api.product.getAll.useQuery()
 
+  const [searchParam, setSearchParam] = useState('')
+  const filteredProducts = useMemo(() => {
+    if (!products) return null
+    if (!searchParam) return products
+    return products.filter(product => product?.name?.includes(searchParam));
+  }, [products, searchParam])
+
   return (
     <>
       <Head>
@@ -30,11 +39,12 @@ export default function Home() {
       <main className="p-5">
         <h1 className="text-xl">Stackfix Live Challenge</h1>
         {/* TODO: Your code here */}
+        <Input placeholder="Search for products" value={searchParam} onChange={(e) => setSearchParam(e.target.value)}/>
         <div className="w-full max-w-4xl">
           {
-            products == null 
+            filteredProducts == null 
               ? <ProductCardSkeleton/>
-              : <ProductCards products={products}/>
+              : <ProductCards products={filteredProducts}/>
           }
         </div>
       </main>
